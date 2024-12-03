cc08804d2e6d948c54e95d09135879e560480667
BadPixxel <eshop.bpaquier@gmail.com>
2024-12-03 17:19:22 +0100

    UP: Template Renderer

--- src/Services/Emails/RawHtmlRenderer.php
+++ src/Services/Emails/RawHtmlRenderer.php
@@ -15,9 +15,7 @@ namespace BadPixxel\BrevoBridge\Services\Emails;
 
 use Exception;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
 
 /**
  * Tooling Service for Rendering Raw Html Contents with Twig
@@ -25,20 +23,9 @@ use Twig\Loader\FilesystemLoader;
 class RawHtmlRenderer
 {
     /**
-     * @var string
-     */
-    const TMPL_DIR = "/var";
-
-    /**
-     * @var string
-     */
-    const TMPL_PATH = "/sib_email_template.html.twig";
-
-    /**
      * Constructor
      */
     public function __construct(
-        private KernelInterface $kernel,
         private Environment $twig
     ) {
     }
@@ -51,20 +38,9 @@ class RawHtmlRenderer
     public function render(string $rawHtml, array $parameters): Response
     {
         //==============================================================================
-        // Push Raw Html as Twig Template
-        $tmplPath = $this->kernel->getProjectDir().self::TMPL_DIR;
-        file_put_contents($tmplPath.self::TMPL_PATH, $rawHtml);
-
-        //==============================================================================
-        // Add Temporary Path to Twig Loader
-        /** @var FilesystemLoader $loader */
-        $loader = $this->twig->getLoader();
-        $loader->addPath($tmplPath);
-
-        //==============================================================================
         // Render Raw Html with Parameter
         return new Response($this->twig->render("@BrevoBridge/Debug/email_view.html.twig", array(
-            "tmplPath" => self::TMPL_PATH,
+            "rawHtml" => $rawHtml,
             'tmplParams' => $parameters,
         )));
     }
@@ -77,19 +53,11 @@ class RawHtmlRenderer
     public function renderRaw(string $rawHtml, array $parameters): string
     {
         //==============================================================================
-        // Push Raw Html as Twig Template
-        $tmplPath = $this->kernel->getProjectDir().self::TMPL_DIR;
-        file_put_contents($tmplPath.self::TMPL_PATH, $this->convert($rawHtml));
-
-        //==============================================================================
-        // Add Temporary Path to Twig Loader
-        /** @var FilesystemLoader $loader */
-        $loader = $this->twig->getLoader();
-        $loader->addPath($tmplPath);
-
-        //==============================================================================
         // Render Raw Html with Parameter
-        return $this->twig->render(self::TMPL_PATH, $parameters);
+        return new Response($this->twig->render("@BrevoBridge/Debug/raw_view.html.twig", array(
+            "rawHtml" => $this->convert($rawHtml),
+            'tmplParams' => $parameters,
+        )));
     }
 
     /**
